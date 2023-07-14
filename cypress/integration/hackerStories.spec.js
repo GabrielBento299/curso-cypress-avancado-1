f1c10baf47d364d141aa90eff598eed40217953a
Gabriel Bento <gabrielbento299@hotmail.com>
2023-07-14 15:54:00 -0300

    done exercise front-end independent from api 2.1

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -74,12 +74,27 @@ describe('Hacker Stories', () => {
       })
 
       context('List of stories', () => {
-        // Since the API is external,
-        // I can't control what it will provide to the frontend,
-        // and so, how can I assert on the data?
-        // This is why this test is being skipped.
-        // TODO: Find a way to test it out.
-        it.skip('shows the right data for all rendered stories', () => {})
+        it.only('shows the right data for all rendered stories', () => {
+          const stories = require('../fixtures/stories')
+
+          cy.get('.item')
+            .first()
+            .should('contain', stories.hits[0].title) 
+            .and('contain', stories.hits[0].author)
+            .and('contain', stories.hits[0].num_comments)
+            .and('contain', stories.hits[0].points)
+          cy.get(`.item a:contains(${stories.hits[0].title})`)
+            .should('have.attr', 'href', stories.hits[0].url)
+
+          cy.get('.item')
+            .last()
+            .should('contain', stories.hits[2].title) 
+            .and('contain', stories.hits[2].author)
+            .and('contain', stories.hits[2].num_comments)
+            .and('contain', stories.hits[2].points)
+          cy.get(`.item a:contains(${stories.hits[2].title})`)
+            .should('have.attr', 'href', stories.hits[2].url)
+          })
 
         it('shows one less story after dimissing the first one', () => {
           cy.get('.button-small')
@@ -151,7 +166,7 @@ describe('Hacker Stories', () => {
       })
 
       context('Last searches', () => {
-        it.only('shows a max of 5 buttons for the last searched terms', () => {
+        it('shows a max of 5 buttons for the last searched terms', () => {
           const faker = require('faker')
 
           cy.intercept(
