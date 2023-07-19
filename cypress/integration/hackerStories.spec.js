1392c41bc841a02452a1e450abcc83a025dc09f6
Gabriel Bento <gabrielbento299@hotmail.com>
2023-07-19 12:04:41 -0300

    done exercise modifing the relatory int tests

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -333,7 +333,7 @@ context('Errors', () => {
   })
 })
 
-it.only('shows a "Loading ..." state before showing the results', () => {
+it('shows a "Loading ..." state before showing the results', () => {
   cy.intercept(
     'GET',
     '**/search**',
