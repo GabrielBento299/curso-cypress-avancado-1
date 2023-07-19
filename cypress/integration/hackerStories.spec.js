30228be20dd19b87398d14a3522abea0ccc05eac
Gabriel Bento <gabrielbento299@hotmail.com>
2023-07-19 11:57:39 -0300

    done exercise modifing the relatory int tests

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -333,3 +333,20 @@ context('Errors', () => {
   })
 })
 
+it.only('shows a "Loading ..." state before showing the results', () => {
+  cy.intercept(
+    'GET',
+    '**/search**',
+    { 
+      delay: 1000,
+      fixture: 'stories'
+    }  
+  ).as('getDelayedStories')
+
+  cy.visit('/')
+
+  cy.assertLoadingIsShownAndHidden()
+  cy.wait('@getDelayedStories')
+
+  cy.get('.item').should('have.length', 3)
+});
